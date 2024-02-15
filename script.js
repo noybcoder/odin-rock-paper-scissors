6f699f0de9493aa13a9f3896a19eb407a0dd0570
Preston Lam <plam@resource-innovations.com>
2024-02-14 21:47:57 -0700

    first commit

--- script.js
+++ script.js
@@ -1,14 +1,1 @@
-const input = document.querySelector('input');
-const button = document.querySelector('button');
-let para = document.querySelector('p');
-
-button.addEventListener('click', () => {
-    const num = parseInt(input.value);
-    for (let i = 1; i <= num; i++) {
-        let sqrtNum = Math.sqrt(i);
-        if (sqrtNum !== Math.floor(sqrtNum)) {
-            continue;
-        } 
-        para.textContent += `${i} `;
-    }
-})
+prompt('what is your name?');
